70b76190f1894cf3225fd7b02e6b4bb9b118f31d
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2023-12-05 19:01:32 +0530

    build

---
+++ build/static/js/4.9592aea7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[4],{921:function(a,s,d){}}]);
+//# sourceMappingURL=4.9592aea7.chunk.js.map
