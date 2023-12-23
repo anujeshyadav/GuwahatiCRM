9c5740d1d71b9fa29086db05f2024b8016037c84
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2023-12-23 19:30:02 +0530

    build

---
+++ build/static/js/397.0ea2e93d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[397],{3087:function(a,e,t){"use strict";t.r(e);var n=t(1),r=t.n(n);e.default=function(){return r.a.createElement("div",null,"wastagematerialproduction")}}}]);
+//# sourceMappingURL=397.0ea2e93d.chunk.js.map
