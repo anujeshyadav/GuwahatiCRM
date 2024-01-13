84eaefbe16fd0718e1a0ac5b92598fe085f3be2a
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2024-01-13 19:26:05 +0530

    build

---
+++ build/static/js/408.0b9f328f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[408],{3099:function(a,t,e){"use strict";e.r(t);var n=e(1),r=e.n(n);t.default=function(){return r.a.createElement("div",null,"wastagestockreturnproduction")}}}]);
+//# sourceMappingURL=408.0b9f328f.chunk.js.map
